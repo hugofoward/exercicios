b17bc0e914f930fc5f661945fe30c645dc44ccff
Hugo Brito <hugo.bh@hotmail.com>
2022-07-05 09:33:35 -0300

    response ofx

--- pages/api/belvoTransactions.js
+++ pages/api/belvoTransactions.js
@@ -3,7 +3,7 @@ export default async(req,res) => {
 var belvo = require('belvo').default;
 var client = new belvo( /* ClientId */ '3f9f5b25-7b96-478b-8ff9-7129e1ffc4e7', /* SecretId */ 'YQDOuSNUnQ0ehD_-lq2Y*#AHa9O3PPDVb1xGItkoG_g9NTf29lebuObvd*@lRd66',/* Environment */ 'development' );
 const transacoes = []
-// OFX - head
+// OFX - head 
 transacoes.push('OFXHEADER:100 DATA:OFXSGML VERSION:102 SECURITY:NONE ENCODING:USASCII CHARSET:1252 COMPRESSION:NONE OLDFILEUID:NONE NEWFILEUID:NONE <OFX><SIGNONMSGSRSV1><SONRS><STATUS><CODE></CODE><SEVERITY>INFO</SEVERITY></STATUS><DTSERVER></DTSERVER><LANGUAGE>POR</LANGUAGE><FI><ORG></ORG><FID></FID></FI></SONRS></SIGNONMSGSRSV1><BANKMSGSRSV1><STMTTRNRS><TRNUID></TRNUID><STATUS><CODE></CODE><SEVERITY>INFO</SEVERITY></STATUS><STMTRS><CURDEF>BRL</CURDEF><BANKACCTFROM><BANKID></BANKID><BRANCHID></BRANCHID><ACCTID></ACCTID><ACCTTYPE></ACCTTYPE></BANKACCTFROM><BANKTRANLIST><DTSTART></DTSTART><DTEND></DTEND>')
 return client.connect()
   .then(async() => {
@@ -22,8 +22,8 @@ return client.connect()
      
         // sendOFX(transacoes)
 
-        console.log(transacoes)
-        res.json(response);
+        // console.log(transacoes)
+        res.json(transacoes);
       })
       .catch(function (error) { 
         res.status(500).send({
